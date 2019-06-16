966869cc919d63bef829dae1b56e0f834a82d51f
min <min.songkyung@gmail.com>
2019-06-16 16:04:52 +0900

    settings

---
+++ VIewModels/SettingsViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewModel.swift
+//  Weather
+//
+//  Created by 민송경 on 15/06/2019.
+//  Copyright © 2019 민송경. All rights reserved.
+//
+
+import Foundation
+
+enum Unit: String {
+    case celsius = "metric" //map with value from json
+    case fahrenheit = "imperial"
+}
+
+struct SettingsViewModel {
+ 
+    let units = 
+}
